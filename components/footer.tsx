5925b7367353627c0e6e80be7bc04fa8ce02e640
ranad4508 <rand4508@gmail.com>
2025-05-21 13:27:55 +0545

    fixed responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,7 +11,7 @@ import {
 export default function Footer() {
   return (
     <footer className="bg-maroon text-white">
-      <div className="container mx-auto px-4 py-12">
+      <div className="mx-auto px-4 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           <div>
             <h3 className="font-lora text-xl mb-4 text-gold">
